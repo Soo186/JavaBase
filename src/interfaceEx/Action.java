1585aab848a8b5ea94250b5032eb2362bfcbc937
Soo186 <soo@Soos-Mac.local>
2024-03-20 09:44:51 +0900

    commit

---
+++ src/interfaceEx/Action.java
@@ -0,0 +1,5 @@
+package interfaceEx;
+
+public interface Action {                  //인터페이스는 기초 설계도 -> 구현 클래스(오버라이드 추상 메서드 구성) ->
+	public void work();
+}
---
+++ src/interfaceEx/ActionEx01.java
@@ -0,0 +1,7 @@
+package interfaceEx;
+
+public class ActionEx01 {
+	public static void main(String[] args) {
+
+	}
+}
